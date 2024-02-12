baec58f98abd27af79efdf1570aa508deaef688a
kevinkliimask <kevinkliimask@gmail.com>
2024-02-12 22:52:22 +0200

    implement editing channels

--- app/api/channel/[channelId]/route.ts
+++ app/api/channel/[channelId]/route.ts
@@ -48,3 +48,56 @@ export async function DELETE(req: Request, { params }: { params: { channelId: st
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
+
+export async function PATCH(req: Request, { params }: { params: { channelId: string } }) {
+  try {
+    const profile = await currentProfile();
+    const { name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId || !params.channelId || name === "general") {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              name: {
+                not: "general",
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
