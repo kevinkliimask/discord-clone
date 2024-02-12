e7966d63828ca3bab6081bf86a20ae1ece878db4
kevinkliimask <kevinkliimask@gmail.com>
2024-02-12 20:47:17 +0200

    implement deleting channels

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react";
 
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
-import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -25,6 +26,7 @@ export const ModalProvider = () => {
     <>
       <CreateChannelModal />
       <CreateServerModal />
+      <DeleteChannelModal />
       <DeleteServerModal />
       <EditServerModal />
       <InviteModal />
